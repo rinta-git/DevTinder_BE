7f1edd0682b367533570da22b56d4d0b2a76557f
Rinta Roy <rintaroyofficial@gmail.com>
2025-08-07 21:47:24 +0200

    Initial setup & created the server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use("/test", (req, res) => {
+    res.send("Register here!");
+})
+
+app.use("/", (req, res) => {
+    res.send("Hello, World!");
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000...');
+});
