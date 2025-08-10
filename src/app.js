745eba2e86a82e0ae8dbd2e9d4134a36cd3be64d
Rinta Roy <rintaroyofficial@gmail.com>
2025-08-10 12:15:45 +0200

    expolring regex in routes

--- src/app.js
+++ src/app.js
@@ -2,27 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.delete("/users", (req, res) => {
-    res.send("deleted successfully");
-})
 
-app.patch("/users", (req, res) => {
-    res.send("updated successfully");
-})
-
-app.put("/users", (req, res) => {
-    res.send("replaced successfully");
-})
-
-app.post("/users", (req, res) => {
-    res.send("successfully added");
-})
 
 app.get("/users", (req, res) => {
     res.send({firstName: "John", lastName: "Doe"});
 })
 
-app.get("/", (req, res) => {
+
+//works with efg and xfff...+nc. Meaning + makes f can
+// be multiple but it can be zero also and then ends with g
+app.get(/ef*g/, (req, res) => {   
+    res.send("Hello, World!");
+})
+
+
+//works with xyc and xyyyy...+nc. Meaning + makes y can
+// be multiple but it should be there atleast once and then ends with c
+app.get(/xy+c/, (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.get(/ab?c/, (req, res) => {   //works with ac and abc. Meaning ? makes b optional
     res.send("Hello, World!");
 })
 
