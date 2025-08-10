7db38575bf2cb5d66440fe61a035edd94b55a97e
Rinta Roy <rintaroyofficial@gmail.com>
2025-08-10 12:02:01 +0200

    understanding http methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const app = express();
 
-
-app.use("/test", (req, res) => {
-    res.send("Register here!");
+app.delete("/users", (req, res) => {
+    res.send("deleted successfully");
 })
 
-app.use("/", (req, res) => {
+app.patch("/users", (req, res) => {
+    res.send("updated successfully");
+})
+
+app.put("/users", (req, res) => {
+    res.send("replaced successfully");
+})
+
+app.post("/users", (req, res) => {
+    res.send("successfully added");
+})
+
+app.get("/users", (req, res) => {
+    res.send({firstName: "John", lastName: "Doe"});
+})
+
+app.get("/", (req, res) => {
     res.send("Hello, World!");
 })
 
