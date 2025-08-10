237f341fc89463a55e4aec1bb9d1cb9929489ffc
Rinta Roy <rintaroyofficial@gmail.com>
2025-08-10 12:20:19 +0200

    understanding query param and dynamic route

--- src/app.js
+++ src/app.js
@@ -3,27 +3,15 @@ const express = require('express');
 const app = express();
 
 
-
-app.get("/users", (req, res) => {
-    res.send({firstName: "John", lastName: "Doe"});
+app.get("/users/", (req, res) => {
+    console.log(req.query) //results what you passed in url. It makes you to pass query param
+    res.send({ firstName: "John", lastName: "Doe", userId });
 })
 
-
-//works with efg and xfff...+nc. Meaning + makes f can
-// be multiple but it can be zero also and then ends with g
-app.get(/ef*g/, (req, res) => {   
-    res.send("Hello, World!");
-})
-
-
-//works with xyc and xyyyy...+nc. Meaning + makes y can
-// be multiple but it should be there atleast once and then ends with c
-app.get(/xy+c/, (req, res) => {
-    res.send("Hello, World!");
-})
-
-app.get(/ab?c/, (req, res) => {   //works with ac and abc. Meaning ? makes b optional
-    res.send("Hello, World!");
+app.get("/users/:userId", (req, res) => {
+    const { userId } = req.params;
+    console.log(userId) //results what you passed in url. It makes route dynamic
+    res.send({ firstName: "John", lastName: "Doe", userId });
 })
 
 
