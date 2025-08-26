49230a9898f95f0ac745bc444a91db93d6ce27fd
Rinta Roy <rintaroyofficial@gmail.com>
2025-08-26 13:37:57 +0200

    understanding dynamic data with post, get using find, findOne

--- src/app.js
+++ src/app.js
@@ -3,14 +3,10 @@ const devTinderDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json()); //middleware to convert json body to javascript object. It runs for all incoming requests.
+
 app.post("/signUp", async (req, res) => {
-  const user = new User({
-    email: "xxxx@gmail.com",
-    password: "xxxpassword",
-    firstName: "xxx",
-    lastName: "xxxUser",
-    gender: "Male",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
@@ -19,6 +15,46 @@ app.post("/signUp", async (req, res) => {
     res.status(500).send("Error signing up user");
   }
 });
+
+app.get("/user", async(req, res) => {
+  const userName = req.body.firstName;
+  try{
+    const resp = await User.findOne({firstName:userName}) //To get one user that has common name
+    if(!resp){
+      return res.status(404).send("Users not found")
+    }
+    res.send(resp)
+  }catch (err){
+    res.status(400).send("Something went wrong!");
+  }
+})
+
+app.get("/user", async(req, res) => {
+  const userEmail = req.body.email
+  try{
+    const resp = await User.find({email: userEmail}) //To get one specific user
+    if(resp.length === 0){
+      return res.status(404).send("User not found")
+    }
+    res.send(resp)
+  }catch (err){
+    res.status(400).send("Something went wrong!");
+  }
+})
+
+app.get("/users", async(req, res) => {
+  try{
+    const resp = await User.find({}) //To get all users
+    if(resp.length === 0){
+      return res.status(404).send("Users not found")
+    }
+    res.send(resp)
+  }catch (err){
+    res.status(400).send("Something went wrong!");
+  }
+})
+
+
 
 devTinderDB()
   .then(() => {
